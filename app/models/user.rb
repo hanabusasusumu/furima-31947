7764ff4b745a8236bc33d34008c28b944eee6193
hanabusasusumu <buckside1080@i.softbank.jp>
2021-01-19 20:11:37 +0900

    ユーザーモデルのバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
     validates :last_name, presence: true
     validates :first_name, presence: true
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カナ文字を使用してください' } do
     validates :last_name_furigana, presence: true
     validates :first_name_furigana, presence: true
   end
