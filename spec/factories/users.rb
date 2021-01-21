ec4edf3b9e2f70942b91cda0d95bfc20cc7ae6e0
hanabusasusumu <buckside1080@i.softbank.jp>
2021-01-21 19:30:01 +0900

    FactoryBotの導入

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :user do
+    name                  {"test"}
+    email                 {"test@example"}
+    password              {"000aaa"}
+    password_confirmation {password}
+    last_name             {"山田"}
+    first_name            {"太郎"}
+    last_name_furigana    {"ヤマダ"}
+    first_name_furigana   {"タロウ"}
+    birthday              {"1930-1-1"}
+  end
