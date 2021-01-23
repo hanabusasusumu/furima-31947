6d2d7779feadfae9b189e0769a305c260b3248e9
hanabusasusumu <buckside1080@i.softbank.jp>
2021-01-23 09:47:54 +0900

    バリデーションの記述を訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
+
   validates :nickname, presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
-    validates :last_name, presence: true
-    validates :first_name, presence: true
+    validates :last_name
+    validates :first_name
   end
+
   with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カナ文字を使用してください' } do
-    validates :last_name_furigana, presence: true
-    validates :first_name_furigana, presence: true
+    validates :last_name_furigana
+    validates :first_name_furigana
   end
-  validates :birthday, presence: true
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
