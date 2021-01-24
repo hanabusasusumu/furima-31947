b5da9bd30250429889f8386b887cab77df3c401b
hanabusasusumu <buckside1080@i.softbank.jp>
2021-01-24 19:18:21 +0900

    Active Hashの導入（カテゴリー欄）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
   belongs_to :user
   has_one_attached :image
+
+  validates :category_id, numericalety: {other_than: 1}
 end
