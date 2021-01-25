f7fbd32d269c8588230943b56c0cfa7ffbcad78f
hanabusasusumu <buckside1080@i.softbank.jp>
2021-01-25 19:35:36 +0900

    「商品の状態」を選択できるように実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,11 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :state
   belongs_to :user
   has_one_attached :image
 
   validates :category_id, numericalety: {other_than: 1}
+  validates :state_id, numericalety: {other_than: 1}
+
 end
