47ddab535aa2a8a429b8673c8b7682d89e46209c
hanabusasusumu <buckside1080@i.softbank.jp>
2021-01-25 20:39:55 +0900

    「発送までの日数」を選択できるように実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to :state
   belongs_to :delivery_fee
   belongs_to :prefecture
+  belongs_to :day
 
   belongs_to :user
   has_one_attached :image
@@ -12,5 +13,5 @@ class Item < ApplicationRecord
   validates :state_id, numericality: {other_than: 1}
   validates :delivery_fee_id, numericality: {other_than: 1}
   validates :prefecture_id, numericality: {other_than: 1}
-
+  validates :day_id, numericality: {other_than: 1}
 end
