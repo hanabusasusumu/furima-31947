0c6b9de2298714f808495865495ac4b3200e394b
hanabusasusumu <buckside1080@i.softbank.jp>
2021-01-25 20:27:56 +0900

    「配送料の負担」と「発送元の地域」を選択できるように実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,15 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :state
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+
   belongs_to :user
   has_one_attached :image
 
-  validates :category_id, numericalety: {other_than: 1}
-  validates :state_id, numericalety: {other_than: 1}
+  validates :category_id, numericality: {other_than: 1}
+  validates :state_id, numericality: {other_than: 1}
+  validates :delivery_fee_id, numericality: {other_than: 1}
+  validates :prefecture_id, numericality: {other_than: 1}
 
 end
