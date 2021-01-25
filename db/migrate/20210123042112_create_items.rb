a938b43cce9c4c0c3351759edd2adf49b4cf80db
hanabusasusumu <buckside1080@i.softbank.jp>
2021-01-25 21:03:15 +0900

    各カラムの制約を記述

--- db/migrate/20210123042112_create_items.rb
+++ db/migrate/20210123042112_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string     :name
-      t.text       :message
-      t.integer    :category_id
-      t.integer    :state_id
-      t.integer    :delivery_fee_id
-      t.integer    :prefecture_id
-      t.integer    :day_id
-      t.integer    :price
-      t.references :user
+      t.string     :name,            null: false  
+      t.text       :message,         null: false
+      t.integer    :category_id,     null: false
+      t.integer    :state_id,        null: false
+      t.integer    :delivery_fee_id, null: false
+      t.integer    :prefecture_id,   null: false
+      t.integer    :day_id,          null: false
+      t.integer    :price,           null: false
+      t.references :user,            foreign_key: true
 
       t.timestamps
     end
