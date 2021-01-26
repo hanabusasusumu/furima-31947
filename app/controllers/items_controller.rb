3916468d549e8234b910104d3a655ccb4c4d95c5
hanabusasusumu <buckside1080@i.softbank.jp>
2021-01-26 19:49:21 +0900

    商品データの保存機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+    :image, :name, :message, :category_id, :state_id, :delivery_fee_id,
+    :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
+  end
+
 end
