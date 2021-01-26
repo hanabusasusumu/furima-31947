ff56d865202306f17e805169cf3ba1000f052dc1
hanabusasusumu <buckside1080@i.softbank.jp>
2021-01-26 21:01:19 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,9 +9,20 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :category_id, numericality: {other_than: 1}
-  validates :state_id, numericality: {other_than: 1}
-  validates :delivery_fee_id, numericality: {other_than: 1}
-  validates :prefecture_id, numericality: {other_than: 1}
-  validates :day_id, numericality: {other_than: 1}
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :message
+  end
+
+  with_options numericality: {other_than: 1} do
+    validates :category_id
+    validates :state_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :day_id
+  end
+
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 end
+
