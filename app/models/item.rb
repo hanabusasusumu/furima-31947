bdc7f76b5260635f40251c7acc0afece00000d6e
hanabusasusumu <buckside1080@i.softbank.jp>
2021-01-28 08:57:05 +0900

    「出品する」ボタンをクリックした時の遷移先を修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -23,6 +23,6 @@ class Item < ApplicationRecord
     validates :day_id
   end
 
-  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :price, presence: true, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 end
 
