346d6b69c9de954fa13e26318cd389b5cf25ed02
hanabusasusumu <buckside1080@i.softbank.jp>
2021-01-30 14:50:46 +0900

    いただいたコメントに対する修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,12 +4,12 @@ FactoryBot.define do
 
     name            { 'テスト' }
     message         { 'テスト' }
-    category_id     { '2' }
-    state_id        { '2' }
-    delivery_fee_id { '2' }
-    prefecture_id   { '2' }
-    day_id          { '2' }
-    price           { '300' }
+    category_id     { 2 }
+    state_id        { 2 }
+    delivery_fee_id { 2 }
+    prefecture_id   { 2 }
+    day_id          { 2 }
+    price           { 300 }
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,16 +10,6 @@ RSpec.describe Item, type: :model do
       it 'すべての欄が入力されていれば出品できる' do
         expect(@item).to be_valid
       end
-
-      it '販売価格が300~9,999,999だと登録できる' do
-        @item.price = '300'
-        expect(@item).to be_valid
-      end
-
-      it '販売価格が半角数字での入力だと登録できる' do
-        @item.price = '300'
-        expect(@item).to be_valid
-      end
     end
 
     context '出品できないとき' do
@@ -42,31 +32,31 @@ RSpec.describe Item, type: :model do
       end
 
       it 'カテゴリーが選択されていないと登録できない' do
-        @item.category_id = '1'
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Category select')
       end
 
       it '商品の状態が選択されていないと登録できない' do
-        @item.state_id = '1'
+        @item.state_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('State select')
       end
 
       it '配送料の負担が選択されていないと登録できない' do
-        @item.delivery_fee_id = '1'
+        @item.delivery_fee_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Delivery fee select')
       end
 
       it '発送元の地域が選択されていないと登録できない' do
-        @item.prefecture_id = '1'
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Prefecture select')
       end
 
       it '発送日までの日数が選択されていないと登録できない' do
-        @item.day_id = '1'
+        @item.day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Day select')
       end
@@ -83,14 +73,26 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
 
+      it '価格が半角英数混合だと登録できない' do
+        @item.price = 'aaa111'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+
+      it '価格が半角英語だけでは登録できない' do
+        @item.price = 'aaaaaa'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+
       it '価格が¥299以下だと登録できない' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than 299')
       end
 
       it '価格が¥10,000,000以上だと登録できない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than 10000000')
       end
