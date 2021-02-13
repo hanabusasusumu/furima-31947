2a4076e1346c341824488a6906d79f5d3bd16f3c
hanabusasusumu <buckside1080@i.softbank.jp>
2021-02-13 14:04:05 +0900

    購入画面を表示できるように設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,18 +2,4 @@ class OrdersController < ApplicationController
   def index
     @user_order = UserOrder.new
   end
-
-  def create
-    @user_order = UserOrder.new(user_order_params)
-    if @user_order.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
-  private
-  def params
-    params.require(:user_order).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone_number)
-  end
 end
--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,6 +1,6 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accesor :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
