e054a42173bf623bc9f8e6523237098119f16475
hanabusasusumu <buckside1080@i.softbank.jp>
2021-02-13 15:50:19 +0900

    購入情報がテーブルに保存されるように設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,5 +5,15 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @user_order = UserOrder.new(order_params)
+    @user_order.save
   end
+
+  private
+
+  def order_params
+    params.require(:user_order).permit(
+      :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number
+    ).merge(user_id: current_user.id, item_id: params[:item_id])
+
 end
