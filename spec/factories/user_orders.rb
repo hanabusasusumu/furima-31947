584d9c8d90bd23933d5dc224f1bb08a762053c0b
hanabusasusumu <buckside1080@i.softbank.jp>
2021-02-18 21:19:23 +0900

    テストコードを書く準備

--- spec/factories/user_orders.rb
+++ spec/factories/user_orders.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user_order do
+    association :user
+    association :item
+
+    postal_code   { 111-1111 } 
+    prefecture_id { 2 }
+    city          { '横浜市緑区' }
+    house_number  { '青山1-1' }
+    phone_number  { 09012345678 }
     
   end
 end
--- spec/models/user_order_spec.rb
+++ spec/models/user_order_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe UserOrder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user_order = FactoryBot.build(:user_order)
+  end
+
+  describe '購入情報' do
+    context '購入できるとき' do
+      it '建物名以外のすべての欄が入力されていれば購入できる' do
+      
+      end
+      it '郵便番号にハイフンがあると購入できる' do
+        
+      end
+      it '電話番号がハイフンなしで11桁以内だと購入できる'
+
+      end
+    end
+
+    context '購入できないとき' do
+      it '郵便番号が空だと購入できない' do
+        
+      end
+      it '郵便番号にハイフンがあると購入できない' do
+      
+      end
+      it '都道府県が選択されていないと購入できない' do
+        
+      end
+      it '市区町村が空だと購入できない' do
+        
+      end
+      it '番地が空だと購入できない' do
+        
+      end
+      it '電話番号が空だと購入できない' do
+        
+      end
+      it '電話番号にハイフンがあり12桁以上だと購入できない' do
+        
+      end
+    end
+  end
 end
