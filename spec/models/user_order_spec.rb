b658b704f3e16ea2641d7e84c0f950f70b4a6205
hanabusasusumu <buckside1080@i.softbank.jp>
2021-02-18 21:56:31 +0900

    テストコードの記述準備

--- spec/models/user_order_spec.rb
+++ spec/models/user_order_spec.rb
@@ -8,34 +8,34 @@ RSpec.describe UserOrder, type: :model do
   describe '購入情報' do
     context '購入できるとき' do
       it '建物名以外のすべての欄が入力されていれば購入できる' do
-      
+
       end
       it '郵便番号にハイフンがあると購入できる' do
-        
+
       end
-      it '電話番号がハイフンなしで11桁以内だと購入できる'
+      it '電話番号がハイフンなしで11桁以内だと購入できる' do
 
       end
     end
 
     context '購入できないとき' do
       it '郵便番号が空だと購入できない' do
-        
+
       end
       it '郵便番号にハイフンがあると購入できない' do
-      
+
       end
       it '都道府県が選択されていないと購入できない' do
-        
+
       end
       it '市区町村が空だと購入できない' do
-        
+
       end
       it '番地が空だと購入できない' do
-        
+
       end
       it '電話番号が空だと購入できない' do
-        
+
       end
       it '電話番号にハイフンがあり12桁以上だと購入できない' do
         
