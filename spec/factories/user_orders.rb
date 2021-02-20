3fd9d125483a1fb8cfd4b428f6439aa9cb9274d3
hanabusasusumu <buckside1080@i.softbank.jp>
2021-02-20 16:57:30 +0900

    テストコードを追加

--- spec/factories/user_orders.rb
+++ spec/factories/user_orders.rb
@@ -6,6 +6,6 @@ FactoryBot.define do
     city          { '横浜市緑区' }
     house_number  { '青山1-1' }
     phone_number  { '09012345678' }
-    
+    token         {'tok_abcdefghijk00000000000000000'}
   end
 end
--- spec/models/user_order_spec.rb
+++ spec/models/user_order_spec.rb
@@ -48,6 +48,12 @@ RSpec.describe UserOrder, type: :model do
         @user_order.valid?
         expect(@user_order.errors.full_messages).to include("Phone number is invalid")
       end
+      it 'tokenが空では購入できない' do
+        @user_order.token = ''
+        @user_order.valid?
+        expect(@user_order.errors.full_messages).to include("Token can't be blank")
+      end
+
     end
   end
 end
